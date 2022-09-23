8dc8d94cee383ac440535545e58363f6942e258d
1053079 <1053079@hr.nl>
2022-09-23 20:46:50 +0200

    Added bullet feature

--- mains.py
+++ mains.py
@@ -8,7 +8,7 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 # Background Image
-background = pygame.image.load("background.png")
+background = pygame.image.load('Background.png')
 
 # Title and icon
 pygame.display.set_caption("Space Invaders")
@@ -28,6 +28,19 @@ enemyY = random.randint (50, 150)
 enemyX_change = 5
 enemyY_change = 20
 
+# Bullet
+
+# Ready = You can't see the bullet on the screen
+# Fire - The bullet is currently moving
+
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 5
+bulletY_change = 20
+bullet_state = "ready"
+
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -35,6 +48,11 @@ def player(x, y):
 
 def enemy(x, y):
     screen.blit(enemyImg, (x, y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 
 # Game Loop
@@ -58,6 +76,10 @@ while running:
 
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
+
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX, bulletY)
+
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -83,6 +105,11 @@ while running:
         enemyX_change = -2
 
         enemyY += enemyY_change
+
+     # Bullet Movement
+    if bullet_state is "fire":
+        fire_bullet(playerX,bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
